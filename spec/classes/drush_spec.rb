8c491646c0a3484db0fa18b7968e86416b8d37d5
Peter Lieverdink <me@cafuego.net>
2015-11-26 15:33:32 +1100

    Also fix the tests if you change the download url and default install destination 😩

--- spec/classes/drush_spec.rb
+++ spec/classes/drush_spec.rb
@@ -4,42 +4,42 @@ describe 'drush', :type => :class do
   let(:title) { 'drush' }
 
   it { should contain_wget__fetch('drush-install') \
-    .with_source('https://github.com/drush-ops/drush/releases/download/8.0.0-rc4/drush.phar') \
+    .with_source('http://files.drush.org/drush.phar') \
     .with_execuser('root') \
-    .with_destination('/usr/local/bin/drush')
+    .with_destination('/usr/bin/drush')
   }
 
   it { should contain_exec('drush-test-download') \
-    .with_command('php /usr/local/bin/drush core-status') \
+    .with_command('php /usr/bin/drush core-status') \
     .with_user('root') \
-    .with_cwd('/usr/local/bin')
+    .with_cwd('/usr/bin')
   }
 
   it { should contain_exec('drush-fix-permissions') \
     .with_command('chmod a+x drush') \
     .with_user('root') \
-    .with_cwd('/usr/local/bin')
+    .with_cwd('/usr/bin')
   }
 
   describe 'with a given target_dir' do
-    let(:params) {{ :target_dir => '/usr/bin' }}
+    let(:params) {{ :target_dir => '/usr/local/bin' }}
 
     it { should contain_wget__fetch('drush-install') \
-      .with_source('https://github.com/drush-ops/drush/releases/download/8.0.0-rc4/drush.phar') \
+      .with_source('http://files.drush.org/drush.phar') \
       .with_execuser('root') \
-      .with_destination('/usr/bin/drush')
+      .with_destination('/usr/local/bin/drush')
     }
 
     it { should contain_exec('drush-test-download') \
-      .with_command('php /usr/bin/drush core-status') \
+      .with_command('php /usr/local/bin/drush core-status') \
       .with_user('root') \
-      .with_cwd('/usr/bin')
+      .with_cwd('/usr/local/bin')
     }
 
     it { should contain_exec('drush-fix-permissions') \
       .with_command('chmod a+x drush') \
       .with_user('root') \
-      .with_cwd('/usr/bin')
+      .with_cwd('/usr/local/bin')
     }
   end
 
@@ -47,21 +47,21 @@ describe 'drush', :type => :class do
     let(:params) {{ :command_name => 'd' }}
 
     it { should contain_wget__fetch('drush-install') \
-      .with_source('https://github.com/drush-ops/drush/releases/download/8.0.0-rc4/drush.phar') \
+      .with_source('http://files.drush.org/drush.phar') \
       .with_execuser('root') \
-      .with_destination('/usr/local/bin/d')
+      .with_destination('/usr/bin/d')
     }
 
     it { should contain_exec('drush-test-download') \
-      .with_command('php /usr/local/bin/d core-status') \
+      .with_command('php /usr/bin/d core-status') \
       .with_user('root') \
-      .with_cwd('/usr/local/bin')
+      .with_cwd('/usr/bin')
     }
 
     it { should contain_exec('drush-fix-permissions') \
       .with_command('chmod a+x d') \
       .with_user('root') \
-      .with_cwd('/usr/local/bin')
+      .with_cwd('/usr/bin')
     }
   end
 
@@ -69,21 +69,21 @@ describe 'drush', :type => :class do
     let(:params) {{ :user => 'pnx' }}
 
     it { should contain_wget__fetch('drush-install') \
-      .with_source('https://github.com/drush-ops/drush/releases/download/8.0.0-rc4/drush.phar') \
+      .with_source('http://files.drush.org/drush.phar') \
       .with_execuser('pnx') \
-      .with_destination('/usr/local/bin/drush')
+      .with_destination('/usr/bin/drush')
     }
 
     it { should contain_exec('drush-test-download') \
-      .with_command('php /usr/local/bin/drush core-status') \
+      .with_command('php /usr/bin/drush core-status') \
       .with_user('pnx') \
-      .with_cwd('/usr/local/bin')
+      .with_cwd('/usr/bin')
     }
 
     it { should contain_exec('drush-fix-permissions') \
       .with_command('chmod a+x drush') \
       .with_user('pnx') \
-      .with_cwd('/usr/local/bin')
+      .with_cwd('/usr/bin')
     }
   end
 end
