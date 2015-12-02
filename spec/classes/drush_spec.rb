cbbeba1557cb09aa839030ae3667053a396a43ef
Peter Lieverdink <me@cafuego.net>
2015-12-02 12:21:54 +1100

    Ensure the tests work.
    * Add composer to fixtures.
    
    * Add tests for versioned install.
    
    * Bump puppet compatibility so File class can be tested.

--- spec/classes/drush_spec.rb
+++ spec/classes/drush_spec.rb
@@ -3,87 +3,112 @@ require 'spec_helper'
 describe 'drush', :type => :class do
   let(:title) { 'drush' }
 
-  it { should contain_wget__fetch('drush-install') \
-    .with_source('http://files.drush.org/drush.phar') \
-    .with_execuser('root') \
-    .with_destination('/usr/bin/drush')
-  }
+  it { should contain_class('drush') }
 
-  it { should contain_exec('drush-test-download') \
-    .with_command('php /usr/bin/drush core-status') \
+  it { should contain_exec('drush-install-7') \
+    .with_command('composer --no-dev require drush/drush 7') \
     .with_user('root') \
-    .with_cwd('/usr/bin')
+    .with_cwd('/opt/drush-7') \
+    .with_environment('COMPOSER_HOME=/opt/drush-7')
   }
 
-  it { should contain_exec('drush-fix-permissions') \
-    .with_command('chmod a+x drush') \
-    .with_user('root') \
-    .with_cwd('/usr/bin')
+  it { should contain_file('/opt/drush-7') \
+    .with_ensure('directory') \
+    .with_owner('root') \
+    .with_mode('0755')
   }
 
-  describe 'with a given target_dir' do
-    let(:params) {{ :target_dir => '/usr/local/bin' }}
+  it { should contain_file('/usr/local/bin/drush') \
+    .with_target('/opt/drush-7/vendor/bin/drush') \
+    .with_owner('root')
+  }
 
-    it { should contain_wget__fetch('drush-install') \
-      .with_source('http://files.drush.org/drush.phar') \
-      .with_execuser('root') \
-      .with_destination('/usr/local/bin/drush')
+  describe 'with a given version' do
+    let(:params) {{ :version => '6' }}
+
+    it { should contain_exec('drush-install-6') \
+      .with_command('composer --no-dev require drush/drush 6') \
+      .with_user('root') \
+      .with_cwd('/opt/drush-6') \
+      .with_environment('COMPOSER_HOME=/opt/drush-6')
     }
 
-    it { should contain_exec('drush-test-download') \
-      .with_command('php /usr/local/bin/drush core-status') \
-      .with_user('root') \
-      .with_cwd('/usr/local/bin')
+    it { should contain_file('/opt/drush-6') \
+      .with_ensure('directory') \
+      .with_owner('root') \
+      .with_mode('0755')
     }
 
-    it { should contain_exec('drush-fix-permissions') \
-      .with_command('chmod a+x drush') \
-      .with_user('root') \
-      .with_cwd('/usr/local/bin')
+    it { should contain_file('/usr/local/bin/drush') \
+      .with_target('/opt/drush-6/vendor/bin/drush') \
+      .with_owner('root')
     }
   end
 
-  describe 'with a given command_name' do
-    let(:params) {{ :command_name => 'd' }}
+  describe 'with a given target_dir' do
+    let(:params) {{ :target_dir => '/usr/local' }}
 
-    it { should contain_wget__fetch('drush-install') \
-      .with_source('http://files.drush.org/drush.phar') \
-      .with_execuser('root') \
-      .with_destination('/usr/bin/d')
+    it { should contain_exec('drush-install-7') \
+      .with_command('composer --no-dev require drush/drush 7') \
+      .with_user('root') \
+      .with_cwd('/usr/local/drush-7') \
+      .with_environment('COMPOSER_HOME=/usr/local/drush-7')
     }
 
-    it { should contain_exec('drush-test-download') \
-      .with_command('php /usr/bin/d core-status') \
-      .with_user('root') \
-      .with_cwd('/usr/bin')
+    it { should contain_file('/usr/local/drush-7') \
+      .with_ensure('directory') \
+      .with_owner('root') \
+      .with_mode('0755')
     }
 
-    it { should contain_exec('drush-fix-permissions') \
-      .with_command('chmod a+x d') \
-      .with_user('root') \
-      .with_cwd('/usr/bin')
+    it { should contain_file('/usr/local/bin/drush') \
+      .with_target('/usr/local/drush-7/vendor/bin/drush') \
+      .with_owner('root')
     }
   end
 
   describe 'with a given user' do
-    let(:params) {{ :user => 'pnx' }}
+    let(:params) {{ :user => 'durpal' }}
 
-    it { should contain_wget__fetch('drush-install') \
-      .with_source('http://files.drush.org/drush.phar') \
-      .with_execuser('pnx') \
-      .with_destination('/usr/bin/drush')
+    it { should contain_exec('drush-install-7') \
+      .with_command('composer --no-dev require drush/drush 7') \
+      .with_user('durpal') \
+      .with_cwd('/opt/drush-7') \
+      .with_environment('COMPOSER_HOME=/opt/drush-7')
     }
 
-    it { should contain_exec('drush-test-download') \
-      .with_command('php /usr/bin/drush core-status') \
-      .with_user('pnx') \
-      .with_cwd('/usr/bin')
+    it { should contain_file('/opt/drush-7') \
+      .with_ensure('directory') \
+      .with_owner('durpal') \
+      .with_mode('0755')
     }
 
-    it { should contain_exec('drush-fix-permissions') \
-      .with_command('chmod a+x drush') \
-      .with_user('pnx') \
-      .with_cwd('/usr/bin')
+    it { should contain_file('/usr/local/bin/drush') \
+      .with_target('/opt/drush-7/vendor/bin/drush') \
+      .with_owner('durpal')
     }
   end
+
+  describe 'with a given command_name' do
+    let(:params) {{ :command_name => '/usr/local/bin/drushicle' }}
+
+    it { should contain_exec('drush-install-7') \
+      .with_command('composer --no-dev require drush/drush 7') \
+      .with_user('root') \
+      .with_cwd('/opt/drush-7') \
+      .with_environment('COMPOSER_HOME=/opt/drush-7')
+    }
+
+    it { should contain_file('/opt/drush-7') \
+      .with_ensure('directory') \
+      .with_owner('root') \
+      .with_mode('0755')
+    }
+
+    it { should contain_file('/usr/local/bin/drushicle') \
+      .with_target('/opt/drush-7/vendor/bin/drush') \
+      .with_owner('root')
+    }
+  end
+
 end
